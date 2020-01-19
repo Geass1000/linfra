490770ca1583e8fbc977d47dc1b95777b6bceb41
Andrey <geass1000@gmail.com>
2020-01-19 04:16:29 +0300

    feat(pkg/arbiter): add `Linfra Metadata` and `Linfra Dependency` interfaces

--- packages/arbiter/src/shared/interfces.ts
+++ packages/arbiter/src/shared/interfces.ts
@@ -2,8 +2,20 @@
 export interface PackageJSON {
   name: string;
   dependencies: PackageJsonDep;
+  linfra?: LinfraMetadata;
+  linfraDeps?: LinfraDependency[];
 }
 
 export interface PackageJsonDep {
   [depName: string]: string;
+}
+
+export interface LinfraMetadata {
+  folderName: string;
+  pathToFolder: string;
+}
+
+export interface LinfraDependency {
+  name: string;
+  linfra: LinfraMetadata;
 }
