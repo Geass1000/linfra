36104e93350d61c77d20bfaceb64bc241d1b0f85
Andrey <geass1000@gmail.com>
2020-01-25 18:42:05 +0300

    feat(pkg/core): add `Array As List` iterator

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,11 @@
 import * as Helpers from './helpers';
+import * as Itertators from './itertators';
 import * as TreeModule from './tree';
+import * as GraphModule from './graph';
 
 export {
   Helpers,
+  Itertators,
   TreeModule,
+  GraphModule,
 };
