6274580e4eaf6b1522c5b57e1ce1d2d8566c694b
Andrey <geass1000@gmail.com>
2020-02-01 19:30:20 +0300

    feat(pkg/arbiter): add `Build Docker Compose Files` logic to `Lerna` arbiter

--- packages/arbiter/src/index.ts
+++ packages/arbiter/src/index.ts
@@ -2,3 +2,4 @@ export * from './pipeline';
 export * from './pipeline.builder';
 export * from './executor';
 export * from './dc-config.builder';
+export * from './lerna.arbiter';
---
+++ packages/arbiter/src/lerna.arbiter.ts
@@ -0,0 +1,102 @@
+import * as _ from 'lodash';
+import * as Bluebird from 'bluebird';
+
+import * as Core from '@linfra/core';
+
+import { Pipeline } from './pipeline';
+import { Executor } from './executor';
+import { DCConfigBuilder } from './dc-config.builder';
+import { Interfaces, Constants } from './shared';
+
+export class LernaArbiter {
+  private pipeline: Pipeline;
+
+  /**
+   * Create instance of LernaArbiter class.
+   *
+   * @return {LernaArbiter}
+   */
+  static create (): LernaArbiter {
+    const inst = new LernaArbiter();
+    return inst;
+  }
+
+  /**
+   * Creates instance of color manager.
+   *
+   * @constructor
+   */
+  constructor () {
+  }
+
+  /**
+   * Sets pipeline.
+   *
+   * @param  {Pipeline} pipeline
+   * @return {void}
+   */
+  setPipeline (pipeline: Pipeline): void {
+    this.pipeline = pipeline;
+  }
+
+  /**
+   * Builds a `Deps`, a `Build` and a `Watch` Docker Compose Yaml files where
+   * packages were built by Docker Compose.
+   *
+   * @param   {Interfaces.LinfraLernaBuildDCFilesConfig} [userConfig]
+   * @returns Promise<void>
+   */
+  async buildDockerComposeFiles (
+    userConfig?: Interfaces.LinfraLernaBuildDCFilesConfig,
+  ): Promise<void> {
+    if (_.isNil(this.pipeline)) {
+      throw new Error (`Pipeline is not installed to the arbiter`);
+    }
+
+    const config: Interfaces.LinfraLernaBuildDCFilesConfig = _.assign(
+      {}, Constants.Default.LinfraLernaBuildDCFilesConfig, userConfig,
+    );
+
+    const pipelineIterator = this.pipeline.getIterator();
+    for (pipelineIterator.start(); !pipelineIterator.isStopped(); pipelineIterator.next()) {
+      const pipelineLevel = pipelineIterator.value;
+
+      console.debug(`LernaArbiter - buildDockerComposeFiles:`,
+        `Handle level ${pipelineIterator.index}`);
+      await Bluebird.map(pipelineLevel, async (linfraModule) => {
+        const dcFilePath = `${linfraModule.pathToFolder}/${config.dcFileName}`;
+        const hasDCFile = await Core.Helpers.FSHelper.hasFile(dcFilePath);
+        if (!hasDCFile) {
+          return;
+        }
+
+        const lmDeps = this.pipeline.getLMDependencies(linfraModule);
+
+        const dcConfigBuilder = DCConfigBuilder.create();
+        const dcBaseYaml = await dcConfigBuilder.openBaseConfigFile(dcFilePath);
+
+        const dcDepsYaml = dcConfigBuilder.createDepsYaml(
+          dcBaseYaml,
+          lmDeps,
+          config.dockerWorkFolderName,
+        );
+        const dcDepsFilePath = `${linfraModule.pathToFolder}/${config.dcDepsFileName}`;
+        await dcConfigBuilder.saveYamlFile(dcDepsFilePath, dcDepsYaml);
+
+        const dcBuildYaml = dcConfigBuilder.createCommandYaml(
+          dcDepsYaml,
+          config.dcBuildCommand,
+        );
+        const dcBuildFilePath = `${linfraModule.pathToFolder}/${config.dcBuildFileName}`;
+        await dcConfigBuilder.saveYamlFile(dcBuildFilePath, dcBuildYaml);
+
+        const dcWatchYaml = dcConfigBuilder.createCommandYaml(
+          dcDepsYaml,
+          config.dcWatchCommand,
+        );
+        const dcWatchFilePath = `${linfraModule.pathToFolder}/${config.dcWatchFileName}`;
+        await dcConfigBuilder.saveYamlFile(dcWatchFilePath, dcWatchYaml);
+      });
+    }
+  }
+}
